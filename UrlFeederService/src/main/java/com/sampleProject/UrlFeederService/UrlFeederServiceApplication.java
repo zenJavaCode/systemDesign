6e6e10b1415bab0b15cea79972c0b6b64c5270d8
Jain Piyush <piyush.j@moxey.ai>
2023-07-20 11:41:32 +0530

    added a sample project to learn docker

---
+++ UrlFeederService/src/main/java/com/sampleProject/UrlFeederService/UrlFeederServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sampleProject.UrlFeederService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlFeederServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlFeederServiceApplication.class, args);
+	}
+
+}
---
+++ UrlFeederService/src/test/java/com/sampleProject/UrlFeederService/UrlFeederServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleProject.UrlFeederService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlFeederServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
